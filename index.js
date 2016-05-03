62e5686fa1d111bdd346aa0a2a926454ec576060
Vania Mass <vaneys@gmail.com>
2016-05-03 14:03:25 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by vaney on 03/05/2016.
+ */
+
+
+module.exports = {
+    HTTPProxyClient: require('./src/HTTPProxyClient')
+};
---
+++ src/HTTPProxyClient.js
@@ -0,0 +1,12 @@
+/**
+ * Created by vaney on 03/05/2016.
+ */
+
+/**
+ * @constructor
+ */
+function HTTPProxyClient() {
+    
+}
+
+module.exports = HTTPProxyClient;
